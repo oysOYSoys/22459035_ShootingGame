8439831f7e6b0442e64c3da3122400c8f144efce
oysOYSoys <oys205103@gmail.com>
2026-04-11 00:16:26 +0900

    26.04.11
    5~6주차 과제 (2주차 과제입니다.)

---
+++ Assets/Scrips/Monster.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public float spd = 1.0f;
+    
+    GameObject target;
+
+    Vector3 direct = Vector3.down;
+
+    public GameObject prefabsExplosion;
+
+    private void Start()
+    {
+        int rndNum = Random.Range(0, 10);
+        if (rndNum < 3)
+        {
+            GameObject target = GameObject.Find("Character");
+            direct= target.transform.position - transform.position;
+            direct.Normalize();
+        }
+    }
+
+    void Update()
+    {
+        transform.Translate(direct * spd * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject explosionObj = Instantiate(prefabsExplosion);
+        explosionObj.transform.position = transform.position;
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
