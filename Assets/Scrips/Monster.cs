389871244cfae1ff8e8ab0334d038213b87385bd
oysOYSoys <oys205103@gmail.com>
2026-04-16 00:55:19 +0900

    7주차 과제 입니다.
    26.04.16

--- Assets/Scrips/Monster.cs
+++ Assets/Scrips/Monster.cs
@@ -30,9 +30,27 @@ public class Monster : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        GameObject explosionObj = Instantiate(prefabsExplosion);
-        explosionObj.transform.position = transform.position;
-        Destroy(collision.gameObject);
-        Destroy(gameObject);
+        if(collision.gameObject.tag == "Bullet")
+        {
+            GameObject gameManager = GameObject.Find("GameManager");
+            ScoreManager scoreManager = gameManager.GetComponent<ScoreManager>();
+            scoreManager.nowScore++;
+            scoreManager.nowScoreUI.text = "Now Score : " + scoreManager.nowScore;
+
+            if(scoreManager.nowScore > scoreManager.bestScore)
+            {
+                scoreManager.bestScore = scoreManager.nowScore;
+                scoreManager.bestScoreUI.text = "Best Score : " + scoreManager.bestScore;
+                PlayerPrefs.SetInt("BestScore", scoreManager.bestScore);
+            }
+
+            GameObject explosionObj = Instantiate(prefabsExplosion);
+            explosionObj.transform.position = transform.position;
+
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+
+        }
+
     }
 }
